2684717902b68621debf2d91bda6b9e924eaffea
juck <juckz@foxmail.com>
2022-12-01 11:52:31 +0800

    feat

--- k3s/setup-master.sh
+++ k3s/setup-master.sh
@@ -13,6 +13,6 @@ TAILSCALE_IP=${MASTER_TAILSCALE_IP}
 # <API Server额外的监听地址>
 EXPOSE_IP=${MASTER_EXPOSE_IP}
 # 这里指定了POD CIDR和 Service CIDR，可以不配置，仅需要保证和内网网段不冲突且互相不冲突即可
-INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC="server --docker --disable servicelb,traefik,local-storage --datastore-endpoint=mysql://${MYSQL_USER}:${MYSQL_PASS}@tcp(${MYSQL_HOST}:3306)/k3s_tx --node-ip $TAILSCALE_IP --advertise-address $TAILSCALE_IP --node-external-ip $TAILSCALE_IP --tls-san $EXPOSE_IP --flannel-iface tailscale0 --cluster-cidr 10.42.0.0/16 --service-cidr 10.43.0.0/16 --kubelet-arg kube-reserved=cpu=100m,memory=100Mi system-reserved=cpu=200m,memory=200Mi --kube-proxy-arg proxy-mode=ipvs masquerade-all=true metrics-bind-address=0.0.0.0" ./install.sh
+INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC="server --disable servicelb,traefik,local-storage --datastore-endpoint=mysql://${MYSQL_USER}:${MYSQL_PASS}@tcp(${MYSQL_HOST}:3306)/k3s_tx --node-ip $TAILSCALE_IP --advertise-address $TAILSCALE_IP --node-external-ip $TAILSCALE_IP --tls-san $EXPOSE_IP --flannel-iface tailscale0 --cluster-cidr 10.42.0.0/16 --service-cidr 10.43.0.0/16 --kubelet-arg kube-reserved=cpu=100m,memory=100Mi system-reserved=cpu=200m,memory=200Mi --kube-proxy-arg proxy-mode=ipvs masquerade-all=true metrics-bind-address=0.0.0.0" ./install.sh
 echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> /etc/profile
 source /etc/profile
--- k3s/setup-node.sh
+++ k3s/setup-node.sh
@@ -13,7 +13,7 @@ EXPOSE_IP=${NODE1_EXPOSE_IP}
 
 # K3S_URL : API Server 的 URL，一般格式为：https://<master_ip>:6443。其中 <master_ip> 是控制节点的公网 IP。
 # K3S_TOKEN : 加入集群所需的 token，可以在控制节点上查看 /var/lib/rancher/k3s/server/node-token
-K3S_URL=${MY_K3S_URL}  K3S_TOKEN=${MY_K3S_TOKEN} INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC="agent --docker --node-ip $TAILSCALE_IP --node-external-ip $TAILSCALE_IP --flannel-iface tailscale0 --kube-proxy-arg proxy-mode=ipvs masquerade-all=true metrics-bind-address=0.0.0.0" ./install.sh
+K3S_URL=${MY_K3S_URL}  K3S_TOKEN=${MY_K3S_TOKEN} INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC="agent --node-ip $TAILSCALE_IP --node-external-ip $TAILSCALE_IP --flannel-iface tailscale0 --kube-proxy-arg proxy-mode=ipvs masquerade-all=true metrics-bind-address=0.0.0.0" ./install.sh
 
 # ==========================
 # ======= racknerd =========
@@ -29,4 +29,4 @@ sudo \cp -f k3s /usr/local/bin/k3s
 
 TAILSCALE_IP=${NODE2_TAILSCALE_IP}
 EXPOSE_IP=${NODE2_EXPOSE_IP}
-K3S_URL=${MY_K3S_URL}  K3S_TOKEN=${MY_K3S_TOKEN} INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC="agent --docker --node-ip $TAILSCALE_IP --node-external-ip $TAILSCALE_IP --flannel-iface tailscale0 --kube-proxy-arg proxy-mode=ipvs masquerade-all=true metrics-bind-address=0.0.0.0" ./install.sh
+K3S_URL=${MY_K3S_URL}  K3S_TOKEN=${MY_K3S_TOKEN} INSTALL_K3S_SKIP_DOWNLOAD=true INSTALL_K3S_EXEC="agent --node-ip $TAILSCALE_IP --node-external-ip $TAILSCALE_IP --flannel-iface tailscale0 --kube-proxy-arg proxy-mode=ipvs masquerade-all=true metrics-bind-address=0.0.0.0" ./install.sh
